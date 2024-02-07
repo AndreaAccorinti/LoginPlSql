cfde2feb8fc1e4b73c9d894bdb83cd62f160773b
ali <aly93akhtar@gmail.com>
2024-02-07 12:33:12 +0100

    add buttons in telegram

--- src/main/java/com/example/loginplsql/Utils/Utils.java
+++ src/main/java/com/example/loginplsql/Utils/Utils.java
@@ -1,11 +1,20 @@
 package com.example.loginplsql.Utils;
 import com.example.loginplsql.models.Presenza;
+import com.example.loginplsql.services.TelegramBot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public final class Utils {
     private static final String PATTERN_FORMAT_DATE = "dd/MM/yyyy";
@@ -15,6 +24,8 @@ public final class Utils {
     private static final String EXIT_M = "/exit_m";
     private static final String ENTRY_P = "/entry_p";
     private static final String EXIT_P = "/exit_p";
+
+    private static Logger log = LoggerFactory.getLogger(TelegramBot.class);
     public static Instant getNowDate() {
         Date date = new Date();
         return new Timestamp(date.getTime()).toInstant();
@@ -29,6 +40,66 @@ public final class Utils {
         if (date == null)
             return "none";
         return DateTimeFormatter.ofPattern(PATTERN_FORMAT_TIME).withZone(ZoneId.systemDefault()).format(date);
+    }
+
+    public static void setDateByResoponse(Presenza presenza, String response) {
+        presenza.setInizioMattina(elaborateMessage(response,ENTRY_M) ?
+                Utils.getNowDate() : presenza.getInizioMattina());
+        presenza.setFineMattina(elaborateMessage(response,EXIT_M) ?
+                Utils.getNowDate() : presenza.getFineMattina());
+        presenza.setInizioPomeriggio(elaborateMessage(response,ENTRY_P) ?
+                Utils.getNowDate() : presenza.getInizioPomeriggio());
+        presenza.setFinePomeriggio(elaborateMessage(response,EXIT_P) ?
+                Utils.getNowDate() : presenza.getFinePomeriggio());
+    }
+
+    public  static void setDescrizioneByResponse(Presenza presenza, String response) {
+        if (elaborateMessage(response,DESC)) {
+            String descrizione = new StringTokenizer(response,DESC).nextToken();
+            if (descrizione != null && descrizione.length() > 0) {
+                presenza.setDescrizione(descrizione);
+            }
+        }
+    }
+
+    public static boolean elaborateMessage(String messageText, String code) {
+        return messageText.contains(code);
+    }
+
+    public static String createBotResponce(Presenza presenza) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("user: "+presenza.getUsername().getUsername()+"\n");
+        sb.append("data: "+ Utils.instantToStringDate(presenza.getData())+"\n");
+        sb.append("entry_m: "+ Utils.instantToStringTime(presenza.getInizioMattina())+"\n");
+        sb.append("exit_m: "+ Utils.instantToStringTime(presenza.getFineMattina())+"\n");
+        sb.append("entry_p: "+ Utils.instantToStringTime(presenza.getInizioPomeriggio())+"\n");
+        sb.append("exit_p: "+ Utils.instantToStringTime(presenza.getFinePomeriggio())+"\n");
+        sb.append("desc: " + presenza.getDescrizione());
+        return sb.toString();
+    }
+
+    public static String processMessage(String messageText) {
+        log.info(messageText);
+        return messageText;
+    }
+
+    public static void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+        KeyboardRow keyboardRowFirstRow = new KeyboardRow();
+        keyboardRowFirstRow.add(ENTRY_M);
+        keyboardRowFirstRow.add(EXIT_M);
+        KeyboardRow keyboardRowSecondRow = new KeyboardRow();
+        keyboardRowSecondRow.add(ENTRY_P);
+        keyboardRowSecondRow.add(EXIT_P);
+        keyboard.add(keyboardRowFirstRow);
+        keyboard.add(keyboardRowSecondRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
     }
 
 }
--- src/main/java/com/example/loginplsql/services/TelegramBot.java
+++ src/main/java/com/example/loginplsql/services/TelegramBot.java
@@ -14,8 +14,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.StringTokenizer;
-
 @Service
 public class TelegramBot extends TelegramLongPollingBot {
     @Autowired
@@ -23,12 +21,6 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Autowired
     PresenzaRepository daoPresenza;
-
-    private final String DESC = "/d";
-    private final String ENTRY_M = "/entry_m";
-    private final String EXIT_M = "/exit_m";
-    private final String ENTRY_P = "/entry_p";
-    private final String EXIT_P = "/exit_p";
     private Logger log = LoggerFactory.getLogger(TelegramBot.class);
     private String token = "6467099780:AAGeFFvVrOqN3Pxj0G5RPqwuDAEnCCJ37VY";
     private String username = "rollcallf2bot";
@@ -47,11 +39,21 @@ public class TelegramBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             Long chatId = update.getMessage().getChatId();
-            // Process the message and generate a response
-            String response = processMessage(messageText);
+            String response = Utils.processMessage(messageText);
             String username = update.getMessage().getFrom().getUserName();
             Presenza presenza = new Presenza();
-            if (response != null && response.length() > 0 && username != null && username.length() > 0) {
+            if (response.equals("/button")) {
+                try {
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setChatId(update.getMessage().getChatId().toString());
+                    Utils.setButtons(sendMessage);
+                    sendMessage.setText("SET UP BUTTONS! :)");
+                    execute(sendMessage);
+                    presenza = null;
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else if (response != null && response.length() > 0 && username != null && username.length() > 0) {
                 User user = null;
                 try {
                     user = this.daoUser.findByUsername(username);
@@ -72,23 +74,11 @@ public class TelegramBot extends TelegramLongPollingBot {
                     if (presenza.getUsername() == null) {
                         presenza.setUsername(user);
                     }
-                    if (elaborateMessage(response,DESC)) {
-                        String descrizione = new StringTokenizer(response,DESC).nextToken();
-                        if (descrizione != null && descrizione.length() > 0) {
-                            presenza.setDescrizione(descrizione);
-                        }
-                    }
+                    Utils.setDescrizioneByResponse(presenza, response);
                     if (presenza.getData() == null) {
                         presenza.setData(Utils.getNowDate());
                     }
-                    presenza.setInizioMattina(elaborateMessage(response,this.ENTRY_M) ?
-                            Utils.getNowDate() : presenza.getInizioMattina());
-                    presenza.setFineMattina(elaborateMessage(response,this.EXIT_M) ?
-                            Utils.getNowDate() : presenza.getFineMattina());
-                    presenza.setInizioPomeriggio(elaborateMessage(response,this.ENTRY_P) ?
-                            Utils.getNowDate() : presenza.getInizioPomeriggio());
-                    presenza.setFinePomeriggio(elaborateMessage(response,this.EXIT_P) ?
-                            Utils.getNowDate() : presenza.getFinePomeriggio());
+                    Utils.setDateByResoponse(presenza, response);
                     try{
                         this.daoPresenza.save(presenza);
                     } catch (Exception e) {
@@ -99,32 +89,13 @@ public class TelegramBot extends TelegramLongPollingBot {
             }
             // Send the response back to the user
             try {
-                execute(new SendMessage(chatId.toString(), createBotResponce(presenza)));
+                if (presenza != null) {
+                    execute(new SendMessage(chatId.toString(), Utils.createBotResponce(presenza)));
+                }
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
-    }
-
-    private String processMessage(String messageText) {
-        log.info(messageText);
-        return messageText;
-    }
-
-    private boolean elaborateMessage(String messageText, String code) {
-        return messageText.contains(code);
-    }
-
-    private String createBotResponce(Presenza presenza) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("user: "+presenza.getUsername().getUsername()+" ");
-        sb.append("data: "+ Utils.instantToStringDate(presenza.getData())+" ");
-        sb.append("entry_m: "+ Utils.instantToStringTime(presenza.getInizioMattina())+" ");
-        sb.append("exit_m: "+ Utils.instantToStringTime(presenza.getFineMattina())+" ");
-        sb.append("entry_p: "+ Utils.instantToStringTime(presenza.getInizioPomeriggio())+" ");
-        sb.append("exit_p: "+ Utils.instantToStringTime(presenza.getFinePomeriggio())+" ");
-        sb.append("desc: " + presenza.getDescrizione());
-        return sb.toString();
     }
 
 }
